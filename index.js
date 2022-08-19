ebf5e35574a70e18307a9e3000fd6a9b98cc67b4
Mary <mcrowe0331@gmail.com>
2022-08-19 10:40:49 -0500

    comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
+//async function and distances
 async function moveNPC(){
     await npc.walkNorth(1400)
     await npc.walkEast(1200)
